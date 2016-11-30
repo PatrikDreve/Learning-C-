ab2a6c18c6b007d5c0f6ea0f8f2b7a03ac5300b5
PatrikDreve <patrik.drevestedt@me.com>
2016-11-30 12:16:56 +0100

    Latest with jump_list

--- Chap02/jump - kopia.cpp
+++ Chap02/jump - kopia.cpp
@@ -1,61 +1,62 @@
-// jump.cpp by Bill Weinman <http://bw.org/>
-// a simple jump table
-// updated for C++ EssT 2014-10-24
+//Jump List Copy by Patrik Drevestedt
+//i övningssyfte (for education purpose)
 
 #include <cstdio>
+
 using namespace std;
 
 const char * prompt();
-int jump( const char * );
+int jump(const char *);
 
-void fa() { puts("this is fa()"); }
-void fb() { puts("this is fb()"); }
-void fc() { puts("this is fc()"); }
-void fd() { puts("this is fd()"); }
-void fe() { puts("this is fe()"); }
+void fa() { puts("This is function fa()"); }
+void fb() { puts("This is function fb()"); }
+void fc() { puts("This is function fc()"); }
+void fd() { puts("This is function fd()"); }
+void fe() { puts("This is function fe()"); }
 
-void (*funcs[])() = { fa, fb, fc, fd, fe, nullptr };
+void(*funcs[])() = { fa, fb, fc, fd, fe, nullptr };
 
-int main( int argc, char ** argv ) {
-    while(jump(prompt())) ;
-    puts("\nDone.");
-    return 0;
+int main(int argc, char ** argv) {
+
+	while (jump(prompt()));
+	puts("\nDone");
+	return 0;
 }
 
 const char * prompt() {
-    puts("Choose an option:");
-    puts("1. Function fa()");
-    puts("2. Function fb()");
-    puts("3. Function fc()");
-    puts("4. Function fd()");
-    puts("5. Function fe()");
-    puts("Q. Quit.");
-    printf(">> ");
-    
-    fflush(stdout);                 // flush after prompt
-    const int buffsz = 16;          // constant for buffer size
-    static char response[buffsz];   // static storage for response buffer
-    fgets(response, buffsz, stdin); // get response from console
-    
-    return response;
+	puts("Choose an option:");
+	puts("1. Function fa()");
+	puts("2. Function fb()");
+	puts("3. Function fc()");
+	puts("4. Function fd()");
+	puts("5. Function fe()");
+	puts("Q. Quit");
+	printf(">> ");
+
+	fflush(stdout);						// flushes after prompt
+	const int buffersz = 16;			// constant for buffersize
+	static char response[buffersz];		// static storage for response buffer
+	fgets(response, buffersz, stdin);	// gets response from console (keyboard)
+
+	return response;
 }
 
-int jump( const char * rs ) {
-    char code = rs[0];
-    if(code == 'q' || code == 'Q') return 0;
-    
-    // count the length of the funcs array
-    int func_length = 0;
-    while( funcs[func_length] != NULL ) func_length++;
-    
-    int i = (int) code - '0';   // convert ASCII numeral to int
-    i--;        // list is zero-based
-    if( i < 0 || i >= func_length ) {
-        puts("invalid choice");
-        return 1;
-    } else {
-        funcs[i]();
-        return 1;
-    }
-    
+int jump(const char * rs) {
+	char code = rs[0];
+	if (code == 'q' || code == 'Q') return 0;
+
+	// count the length of the funcs array
+	int funcLength = 0;
+	while (funcs[funcLength] != NULL) funcLength++;
+
+	int i = (int)code - '0';	// convert ASCII numeral to int
+	i--;	//list is zero based
+		if (i < 0 || i >= funcLength) {
+			puts("Invalid choice");
+			return 1;
+		}
+		else {
+			funcs[i]();
+			return 1;
+		}
 }
