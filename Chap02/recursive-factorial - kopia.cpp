c03e0ff0654ead6014e21e1792cfdc1457bd4f25
PatrikDreve <patrik.drevestedt@me.com>
2016-12-02 13:11:41 +0100

    Latest commit with "recursive-factorial" file

---
+++ Chap02/recursive-factorial - kopia.cpp
@@ -0,0 +1,16 @@
+//recursive-factorial copy, by Patrik Drevestedt
+//for educational purposes, only
+
+#include <cstdio>
+
+using namespace std;
+
+unsigned long int factorial(unsigned long int n) {
+	if (n < 2) return 1;
+	return factorial(n - 1) * n;
+}
+
+int main(int argc, char ** argv) {
+	unsigned long int n = 1;
+	printf("Factorial of %ld is: %ld\n", n, factorial(n));
+}
